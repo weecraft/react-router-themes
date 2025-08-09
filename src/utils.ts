5e667e7a9d8fc87cbf9c74cc8b9e0630a17cfeca
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-09 22:56:46 +0800

    feat: add utils file

---
+++ src/utils.ts
@@ -0,0 +1,67 @@
+interface ScriptProps {
+  attribute: string | string[]
+  storageKey: string
+  defaultTheme: string
+  forcedTheme?: string
+  themes: string[]
+  value?: Record<string, string>
+  enableSystem: boolean
+  enableColorScheme: boolean
+}
+
+export function script({
+  attribute,
+  storageKey,
+  defaultTheme,
+  forcedTheme,
+  themes,
+  value,
+  enableSystem,
+  enableColorScheme,
+}: ScriptProps) {
+  const el = document.documentElement
+  const systemThemes = ["light", "dark"]
+
+  function updateDOM(theme: string) {
+    const attributes = Array.isArray(attribute) ? attribute : [attribute]
+
+    attributes.forEach((attr) => {
+      const isClass = attr === "class"
+      const classes =
+        isClass && value ? themes.map((t) => value[t] || t) : themes
+      if (isClass) {
+        el.classList.remove(...classes)
+        el.classList.add(value && value[theme] ? value[theme] : theme)
+      } else {
+        el.setAttribute(attr, theme)
+      }
+    })
+
+    setColorScheme(theme)
+  }
+
+  function setColorScheme(theme: string) {
+    if (enableColorScheme && systemThemes.includes(theme)) {
+      el.style.colorScheme = theme
+    }
+  }
+
+  function getSystemTheme() {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light"
+  }
+
+  if (forcedTheme) {
+    updateDOM(forcedTheme)
+  } else {
+    try {
+      const themeName = localStorage.getItem(storageKey) || defaultTheme
+      const isSystem = enableSystem && themeName === "system"
+      const theme = isSystem ? getSystemTheme() : themeName
+      updateDOM(theme)
+    } catch (e) {
+      //
+    }
+  }
+}
