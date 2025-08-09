be80ec62fc07c1519a3957c8a83fa15e7b6f9c3f
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-09 22:58:12 +0800

    refactor: add configurations

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,6 +4,12 @@ export default defineConfig([
   {
     entry: ["./src/index.ts"],
     platform: "neutral",
+    sourcemap: false,
+    minify: true,
+    bundle: true,
     dts: true,
+    clean: true,
+    format: ["es"],
+    external: ["react"],
   },
 ])
